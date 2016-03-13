18301b3e6c9fdd7542001fd43374f50599469cda
Billy Tobon <btobon@renttherunway.com>
2016-03-13 18:38:05 -0400

    Project init

---
+++ JSONSchemaValidatorKit/JSONSchemaValidatorKit/JSONSchemaValidatorKit.h
@@ -0,0 +1,19 @@
+//
+//  JSONSchemaValidatorKit.h
+//  JSONSchemaValidatorKit
+//
+//  Created by Billy Tobon on 3/13/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JSONSchemaValidatorKit.
+FOUNDATION_EXPORT double JSONSchemaValidatorKitVersionNumber;
+
+//! Project version string for JSONSchemaValidatorKit.
+FOUNDATION_EXPORT const unsigned char JSONSchemaValidatorKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JSONSchemaValidatorKit/PublicHeader.h>
+
+
